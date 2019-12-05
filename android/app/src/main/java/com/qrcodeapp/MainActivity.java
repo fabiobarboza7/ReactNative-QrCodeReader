be84962503d859674ace677d0dd7e84f75e57af5
Fábio Souza <fabiobarboza7@hotmail.com>
2019-12-05 20:38:55 -0300

    adding scaffold

--- android/app/src/main/java/com/qrcodeapp/MainActivity.java
+++ android/app/src/main/java/com/qrcodeapp/MainActivity.java
@@ -1,6 +1,7 @@
-package com.qrcodeapp;
-
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +12,15 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "qrCodeApp";
+  }
+
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegate(this, getMainComponentName()) {
+      @Override
+      protected ReactRootView createRootView() {
+        return new RNGestureHandlerEnabledRootView(MainActivity.this);
+      }
+    };
   }
 }
